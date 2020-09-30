89511ea83a829ffe4e07ade765cbc91d144a5ef5
Rajat <mrajat67@yahoo.com>
2020-09-30 12:55:51 +0530

    Actual Assignment-1

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -11,6 +11,43 @@ class DishDetail extends Component {
   constructor(props) {
     super(props);
   }
+  renderDish(selectedDish) {
+    return (
+      <Card>
+        <CardImg
+          width="100%"
+          src={selectedDish.image}
+          alt={selectedDish.name}
+        />
+        <CardBody>
+          <CardTitle>{selectedDish.name}</CardTitle>
+          <CardText>{selectedDish.description}</CardText>
+        </CardBody>
+      </Card>
+    );
+  }
+  renderComments(comments) {
+    if (comments === null) {
+      return <div></div>;
+    } else {
+      return (
+        <ul className="list-unstyled">
+          {comments.map((comment) => {
+            return (
+              <div key={comments.id}>
+                <li>{comment.comment}</li>
+                <br></br>
+                <li>
+                  -- {comment.author},{comment.date}
+                </li>
+                <br></br>
+              </div>
+            );
+          })}
+        </ul>
+      );
+    }
+  }
   render() {
     var selectedDish = this.props.selectedDish;
     if (selectedDish === null) {
@@ -20,30 +57,11 @@ class DishDetail extends Component {
       <div>
         <div className="row">
           <div className="col-12 col-md-5 m-1">
-            <Card>
-              <CardImg
-                width="100%"
-                src={selectedDish.image}
-                alt={selectedDish.name}
-              />
-              <CardBody>
-                <CardTitle>{selectedDish.name}</CardTitle>
-                <CardText>{selectedDish.description}</CardText>
-              </CardBody>
-            </Card>
+            {this.renderDish(selectedDish)}
           </div>
           <div className="col-12 col-md-5 m-1">
-            <h3>Comments</h3>
-            {selectedDish.comments.map((comment) => {
-              return (
-                <div key={comment.id}>
-                  <p>{comment.comment}</p>
-                  <p>
-                    --{comment.author},{comment.date}
-                  </p>
-                </div>
-              );
-            })}
+            <h4>Comments</h4>
+            {this.renderComments(selectedDish.comments)}
           </div>
         </div>
       </div>
